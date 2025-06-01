f819e7e0756aef2c78874a157d61899464aa844d
Aditya <adityapawar826@gmail.com>
2025-06-01 11:28:28 +0530

    Observable data with streaming example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,19 @@ export class AppComponent {
   //1. CREATE AN Observable
   // this is an obervable
   myObservable=new Observable((observer)=>{
-    observer.next([1,2,3,4,5]);
+   setTimeout(()=>{observer.next(1)},1000) ;
+   setTimeout(()=>{observer.next(2)},2000) ;
+   setTimeout(()=>{observer.next(3)},3000) ;
+   setTimeout(()=>{observer.next(4)},4000) ;
+   setTimeout(()=>{observer.next(5)},5000) ;
+
   })
 
   onAsynData(){
     //2. This is Observer
     // takes 3 callback fun next, error, complete
   this.myObservable.subscribe((val:any)=>{
-    this.data=val;
+    this.data.push(val);
   })
   }
 }
