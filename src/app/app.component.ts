cbc1fe633ba0fea8c1cc5a85ef856fcc1e9832ca
Aditya <adityapawar826@gmail.com>
2025-06-01 11:23:24 +0530

    Observable working by sending data at once

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularObservables';
+
+  data:any[]=[];
+
+  //1. CREATE AN Observable
+  // this is an obervable
+  myObservable=new Observable((observer)=>{
+    observer.next([1,2,3,4,5]);
+  })
+
+  onAsynData(){
+    //2. This is Observer
+    // takes 3 callback fun next, error, complete
+  this.myObservable.subscribe((val:any)=>{
+    this.data=val;
+  })
+  }
 }
