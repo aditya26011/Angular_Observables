77b11bf3e2959daedc6316937d884146c7f8760b
Aditya <adityapawar826@gmail.com>
2025-06-01 13:04:21 +0530

    Error msg and handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,8 @@ export class AppComponent {
    setTimeout(()=>{observer.next(1)},1000) ;
    setTimeout(()=>{observer.next(2)},2000) ;
    setTimeout(()=>{observer.next(3)},3000) ;
+      setTimeout(()=>{observer.error(new Error('Something went wrong please check after some time.'))},3000) ;
+
    setTimeout(()=>{observer.next(4)},4000) ;
    setTimeout(()=>{observer.next(5)},5000) ;
 
@@ -27,6 +29,8 @@ export class AppComponent {
     // takes 3 callback fun next, error, complete
   this.myObservable.subscribe((val:any)=>{
     this.data.push(val);
+  },(err)=>{
+    alert(err.message)
   })
   }
 }
