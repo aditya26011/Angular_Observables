3b22d3293b3fdb7c7f5b4930421902612e28b7fb
Aditya <adityapawar826@gmail.com>
2025-06-01 13:10:03 +0530

    Complete callback with eg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,12 @@ export class AppComponent {
    setTimeout(()=>{observer.next(1)},1000) ;
    setTimeout(()=>{observer.next(2)},2000) ;
    setTimeout(()=>{observer.next(3)},3000) ;
-      setTimeout(()=>{observer.error(new Error('Something went wrong please check after some time.'))},3000) ;
-
+      // setTimeout(()=>{observer.error(new Error('Something went wrong please check after some time.'))},3000) ;
    setTimeout(()=>{observer.next(4)},4000) ;
    setTimeout(()=>{observer.next(5)},5000) ;
+     setTimeout(()=>{
+      observer.complete()
+    },6000)
 
   })
 
@@ -31,6 +33,9 @@ export class AppComponent {
     this.data.push(val);
   },(err)=>{
     alert(err.message)
-  })
-  }
+  },()=>{
+    alert("All the data is streamed")
+  });
+}
+
 }
