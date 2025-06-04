a2e25d51150ba4edf0fe6e095a597abeb1e0ac0f
Aditya <adityapawar826@gmail.com>
2025-06-04 23:04:13 +0530

    Use of Subject instead of eventEmitter

--- src/app/Services/task.services.ts
+++ src/app/Services/task.services.ts
@@ -1,15 +1,18 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable({
  providedIn: 'root'    
 })
 export class TaskServices{
 
-CreateTask:EventEmitter<string>=new EventEmitter<string>();
+// CreateTask:EventEmitter<string>=new EventEmitter<string>();
+ 
+CreateTask=new Subject<string>();
 
 onCreateTask(val:string){
     console.log(val)
- this.CreateTask.emit(val);
+ this.CreateTask.next(val);
 }
 
 }
