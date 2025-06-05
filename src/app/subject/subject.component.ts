74add75325947abe3b4bd40e9f69b949d77ade80
Aditya <adityapawar826@gmail.com>
2025-06-05 21:14:43 +0530

    Ajax and subscriber consuming data

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-
+import {ajax} from 'rxjs/ajax'
 @Component({
   selector: 'app-subject',
   templateUrl: './subject.component.html',
@@ -10,20 +10,30 @@ export class SubjectComponent implements OnInit {
 ngOnInit(){
 // let myObs=new Observable((observer)=>{observer.next(Math.random())});
 
-const sub=new Subject();
+// const sub=new Subject();
 
 
-//subscriber 1
-sub.subscribe((data)=>{
-  console.log(data)
-})
+// //subscriber 1
+// sub.subscribe((data)=>{
+//   console.log(data)
+// })
 
-//subscriber
-sub.subscribe((data)=>{
-  console.log(data)
-})
+// //subscriber
+// sub.subscribe((data)=>{
+//   console.log(data)
+// })
 
-sub.next(Math.random())
+// sub.next(Math.random())
+
+const subject=new Subject();
+const data=ajax('https://randomuser.me/api/');
+
+subject.subscribe((data)=>{console.log(data)})
+subject.subscribe((data)=>{console.log(data)})
+subject.subscribe((data)=>{console.log(data)})
+
+//subject consuming data
+data.subscribe(subject);
 
 }
 }
