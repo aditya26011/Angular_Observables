4bb7959ddece6b45816677945734ea6b91bdef6f
Aditya <adityapawar826@gmail.com>
2025-06-10 22:10:21 +0530

    Async subject

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 import {ajax} from 'rxjs/ajax'
 @Component({
   selector: 'app-subject',
@@ -10,29 +10,29 @@ export class SubjectComponent implements OnInit {
 ngOnInit(){
 // let myObs=new Observable((observer)=>{observer.next(Math.random())});
 
-const sub=new ReplaySubject(2);
+// const sub=new ReplaySubject(2);
 
-sub.next(100);
-sub.next(200);
-sub.next(300);
-//subscriber 1
-sub.subscribe((data)=>{
-  console.log("subscriber 1 " + data)
-})
+// sub.next(100);
+// sub.next(200);
+// sub.next(300);
+// //subscriber 1
+// sub.subscribe((data)=>{
+//   console.log("subscriber 1 " + data)
+// })
 
-//subscriber
-sub.subscribe((data)=>{
-  console.log("subscriber 2 " + data)
-})
+// //subscriber
+// sub.subscribe((data)=>{
+//   console.log("subscriber 2 " + data)
+// })
 
-sub.next(2020)
+// sub.next(2020)
 
 //subscriber 3
-sub.subscribe((data)=>{
-  console.log("subscriber 3 " + data)//last emitted will be 200 for this
-})
+// sub.subscribe((data)=>{
+//   console.log("subscriber 3 " + data)//last emitted will be 200 for this
+// })
 
-sub.next(400);
+// sub.next(400);
 
 // const subject=new Subject();
 // const data=ajax('https://randomuser.me/api/');
@@ -45,5 +45,22 @@ sub.next(400);
 // data.subscribe(subject);
 
 // }
+
+
+//Async Subject
+const asyncSub=new AsyncSubject();
+
+asyncSub.next(200);
+asyncSub.next(100);
+asyncSub.next(300);
+
+asyncSub.subscribe((data)=>console.log(data))
+
+asyncSub.complete();
+
+asyncSub.subscribe((data)=>console.log(data))
+
+asyncSub.next(400);
+
 }
 }
